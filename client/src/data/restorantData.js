21a668217db3cc3c957b5f862779dfd3c608bd77
raoufbecher <raouf-94@live.fr>
2021-11-15 16:36:19 +0100

    add

--- client/src/data/restorantData.js
+++ client/src/data/restorantData.js
@@ -1,38 +1,40 @@
-
 const restaurants = [
-    {
-        _id:'1',
-        name:"TACOS'619",
-        category:"tacos",
-        placement:"Sahloul",
-        image:"https://scontent.ftun16-1.fna.fbcdn.net/v/t1.6435-9/141667246_261802068734530_1280585996473218421_n.png?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=JydIgCtix9AAX8PjOx3&_nc_ht=scontent.ftun16-1.fna&oh=737fe8f8e22fd383f1eada0fc7b9f3b2&oe=6185F524",
-        phone:"31 185 183",
-        temps:'Commander vos repas de 12:00 à 00:00'
-    },
-    {
-        _id:'2',
-        name:"TEXAS FOOD",
-        category:"Sandwichs",
-        placement:"Cité Riadh",
-        image:"https://scontent.ftun16-1.fna.fbcdn.net/v/t1.6435-9/219571378_1012211982863799_8580546014722118547_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=BCeTwA25tXoAX8K20EK&_nc_ht=scontent.ftun16-1.fna&oh=667b76d940fb63b928b3956375b6c2fd&oe=61893A2D",
-        phone:"36 300 649",
-        temps:'Commander vos repas de 11:00 à 22:00'
-    },
-    {
-        _id:'3',
-        name:"Taxi Pizza",
-        category:"Pizza",
-        placement:"Khzema",
-        image:"https://zesttunisia.com//wp-content/uploads/2018/09/13939345_935140649946918_3700284716259502702_n.jpg",
-        phone:"20 110 022",
-        temps:'Commander vos repas de 12:00 à 00:00'
-    },
-    // {
-    //     _id:'4',
-    //     name:"Chaneb",
-    //     category:"Sandwichs",
-    //     placement:"Cité Riadh",
-    //     image:"https://10619-2.s.cdn12.com/rests/original/109_508589075.jpg"
-    // },
+  {
+    _id: "1",
+    name: "TACOS'619",
+    category: "tacos",
+    placement: "Sahloul",
+    image:
+      "https://scontent.ftun15-1.fna.fbcdn.net/v/t1.6435-9/141667246_261802068734530_1280585996473218421_n.png?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=ILAoJwgwYD4AX-ndyGM&_nc_ht=scontent.ftun15-1.fna&oh=89fcc1ce6615ec2dc30b6c671923f921&oe=61B95FA4",
+    phone: "31 185 183",
+    temps: "Commander vos repas de 12:00 à 00:00",
+  },
+  {
+    _id: "2",
+    name: "TEXAS FOOD",
+    category: "Sandwichs",
+    placement: "Cité Riadh",
+    image:
+      "https://scontent.ftun15-1.fna.fbcdn.net/v/t1.6435-9/75474230_1677453175730270_2972079083493523456_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=gN8pBem4Hi4AX-wOSvL&_nc_ht=scontent.ftun15-1.fna&oh=11794d389b605f1f1b97ffdfa94c72df&oe=61B9C327",
+    phone: "36 300 649",
+    temps: "Commander vos repas de 11:00 à 22:00",
+  },
+  {
+    _id: "3",
+    name: "Taxi Pizza",
+    category: "Pizza",
+    placement: "Khzema",
+    image:
+      "https://zesttunisia.com//wp-content/uploads/2018/09/13939345_935140649946918_3700284716259502702_n.jpg",
+    phone: "20 110 022",
+    temps: "Commander vos repas de 12:00 à 00:00",
+  },
+  // {
+  //     _id:'4',
+  //     name:"Chaneb",
+  //     category:"Sandwichs",
+  //     placement:"Cité Riadh",
+  //     image:"https://10619-2.s.cdn12.com/rests/original/109_508589075.jpg"
+  // },
 ];
 export default restaurants;
